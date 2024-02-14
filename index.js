ab3ea2a8580323b574b54704347994b5b029b576
MariusBaldovin <youremailid@example.com>
2024-02-14 23:08:53 +0000

    Initial commit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const dns = require("dns");
-const { nanoid } = require("nanoid");
 const app = express();
 
 // Basic Configuration
